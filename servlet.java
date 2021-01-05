44b105e72d6878c8c265e4a5b08e13fe04f21410
keshavbathla <48722575+keshavbathla@users.noreply.github.com>
2021-01-05 22:30:02 +0530

    Update servlet.java

--- servlet.java
+++ servlet.java
@@ -1,6 +1,7 @@
 import javax.servlet.*;
 import java.io.*;
 import java.sql.*;
+import java.util.regex.*;
 public class servlet implements Servlet{
 	public void init(ServletConfig h){}
 	public void service(ServletRequest req,ServletResponse res) throws ServletException, IOException{
@@ -12,6 +13,8 @@ public class servlet implements Servlet{
 		String vpass=req.getParameter("verifypass");	
 		String email=req.getParameter("emailname");	
 		String contact=req.getParameter("contactname");	
+		 
+				  
 		try{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","system");
@@ -22,9 +25,51 @@ public class servlet implements Servlet{
 			ps.setString(4,email);
 			ps.setString(5,contact);
 			ps.executeUpdate();
+			if(contact.isEmpty())
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+				pw.println("<br> **Enter Contact Number**");
+			}
+			
+			if(name.isEmpty())
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+				pw.println("<br> **Enter  Name or **");
+			}
+			if(email.isEmpty())
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+			 pw.println("<br> **Enter Email Number**");	
+			}
+				  
+			
+			if(pass.isEmpty())
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+				pw.println("<br> **Enter Password**");
+			}
+			if(vpass.isEmpty())
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+				pw.println("<br> **Enter Verify Password**");
+			}
+			if(pass.equals(vpass))
+			{
 			RequestDispatcher dispatcher=req.getRequestDispatcher("index.html");
 			dispatcher.include(req,res);
 			pw.println("<br>hey "+name+" you are registered succesfully");
+			}
+			else
+			{
+				RequestDispatcher dispatcher=req.getRequestDispatcher("signup.html");
+			dispatcher.include(req,res);
+			pw.println("<br>hey Password Mismatch");
+		    }
 		}
 		catch(Exception e){}
 		pw.close();
